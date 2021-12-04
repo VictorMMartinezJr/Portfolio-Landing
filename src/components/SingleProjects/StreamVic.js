30f9867d643b1561978dd9109ac833427f54f5cf
VictorMtzCodes <victormtzcodes@gmail.com>
2021-12-04 00:21:22 -0500

    created assets folder for images and updated streamVic.js  with data from raw data file

--- src/components/SingleProjects/StreamVic.js
+++ src/components/SingleProjects/StreamVic.js
@@ -2,6 +2,7 @@ import './SingleProject.css';
 import { useRef } from 'react';
 import ProjectImagesCarousel from './ProjectImagesCarousel';
 import ContactMe from '../ContactMe/ContactMe';
+import { streamVicData } from '../../ProjectsData';
 
 const StreamVic = () => {
     const featuresSection = useRef(null);
@@ -10,15 +11,15 @@ const StreamVic = () => {
     }
     return (
         <section className='single-project'>
-            <img className='single-project-img' src="./images/streamvic-project.png" alt="" />
+            <img className='single-project-img' src={streamVicData.headImage} alt="" />
             <div data-aos='fade-up' className='project-titles'>
                 <h1 className='project-title'>StreamVic</h1>
                 <h1 className='project-title-back'>StreamVic</h1>
                 <i className="far fa-arrow-alt-circle-down arrow-down" onClick={goToFeatures}></i>
             </div>
             <div className='single-project-overview'>
-                <h1>Overview</h1>
-                <p className='single-project-p'>StreamVic is a web app built with ReactJs that allows users to browse movie/tv show information and save their favorites. This application uses the TMDB API to fetch movie/tv show data and outputs content dynamically to the webpage. Users can use the API to also search for a particular movie/tv show.</p>
+                <h1>{streamVicData.Overview.title}</h1>
+                <p className='single-project-p'>{streamVicData.Overview.text}</p>
                 <span className='title-technologies'>
                     <p className='title-technology'>React</p>
                     <p className='title-technology'>HTML5</p>
@@ -29,40 +30,40 @@ const StreamVic = () => {
             <div ref={featuresSection} className='single-project-features'>
                 <h1 className='features-title'>Features</h1>
                 <div className='feature-no-photo'>
-                    <h1>Filter</h1>
-                    <p className='single-project-p'>The filter component allows the user to display movies/tv shows according to popularity or top rated. This feature required me to manipulate the TMDB API with the useState hook.</p>
+                    <h1>{streamVicData.Filter.title}</h1>
+                    <p className='single-project-p'>{streamVicData.Filter.text}</p>
                 </div>
                 <div className='feature-with-photo'>
                     <div className='feature-with-photo-info'>
-                        <h1>Pagination</h1>
-                        <p className='single-project-p'>The pagination component allows the user to display different pages of movies/tv shows from the filter they choose. With 4 pages total, the end result is 80 total movies/tv shows with 20 results per page. The component can be used by clicking the prev/next buttons or the dots which glow red if that current page is active.</p>
-                        <p className='single-project-p'>The previous arrow gets disabled if the page is one & the next arrow also gets disabled if the page is 4. Props are used in this component to get the data needed such as page, maxPages, handlePrevPage/handleNextPage functions etc.</p>
+                        <h1>{streamVicData.Pagination.title}</h1>
+                        <p className='single-project-p'>{streamVicData.Pagination.textOne}</p>
+                        <p className='single-project-p'>{streamVicData.Pagination.textTwo}</p>
                     </div>
-                    <img src="images/streamvic-pagination.png" alt="streamvic-pagination" />
+                    <img src={streamVicData.Pagination.image} alt="streamvic-pagination" />
                 </div>
                 <div className='feature-no-photo'>
-                    <h1>Favorites</h1>
-                    <p className='single-project-p'>The favorites component was made with a global state. Favorites was placed in a context file and saved in a createContext() for all other components to use. The original state of the favorites is in the localStorage or an empty array if the localStorage is empty.</p>
+                    <h1>{streamVicData.Favorites.title}</h1>
+                    <p className='single-project-p'>{streamVicData.Favorites.text}</p>
                 </div>
                 <div className='feature-with-photo'>
                     <div className='feature-with-photo-info'>
-                        <h1>Search</h1>
-                        <p className='single-project-p'>The search page was created to allow users to search for any movie or tv show in the TMDB database according to which tab they choose. The movie tab will search for movies & the tv shows tab will search for shows of the search term.</p>
-                        <p className='single-project-p'>The input field value is placed in the TMDB API search url dynamically after the form is submitted. If the form is submitted without an input value, an error message will appear to let the user know the value is empty. There is also an error message for a search value that doesn't have any results in the database.</p>
+                        <h1>{streamVicData.Search.title}</h1>
+                        <p className='single-project-p'>{streamVicData.Search.textOne}</p>
+                        <p className='single-project-p'>{streamVicData.Search.textTwo}</p>
                     </div>
-                    <img src="images/streamvic-search.png" alt="streamvic-search" />
+                    <img src={streamVicData.Search.image} alt="streamvic-search" />
                 </div>
                 <div className='feature-with-photo'>
                     <div className='feature-with-photo-info'>
-                        <h1>Details Page</h1>
-                        <p className='single-project-p'>The details page is a page that shows relevant information about the movie or tv show you clicked on. I used useParams from react-router-dom to dynamically add the ID of the movie or show the user clicks on to the end of the webpage url and also to insert the ID in the details URL of the TMDB API.</p>
-                        <p className='single-project-p'>Details such as an overview, rating, genres, trailer, and cast info is displayed on the page. The option to favorite the movie/show is also avaliable on the details page.</p>
+                        <h1>{streamVicData.Details.title}</h1>
+                        <p className='single-project-p'>{streamVicData.Details.textOne}</p>
+                        <p className='single-project-p'>{streamVicData.Details.textTwo}</p>
                     </div>
-                    <img src="images/streamvic-details.png" alt="streamvic-details" />
+                    <img src={streamVicData.Details.image} alt="streamvic-details" />
                 </div>
             </div>
 
-            <ProjectImagesCarousel />
+            <ProjectImagesCarousel imageOne={streamVicData.CarouselImages.imageOne} imageTwo={streamVicData.CarouselImages.imageTwo} imageThree={streamVicData.CarouselImages.imageThree} />
             <div className='contact-div'>
                 <ContactMe />
                 <i className="far fa-arrow-alt-circle-up arrow-up" onClick={() => window.scrollTo(0, 0)}></i>
