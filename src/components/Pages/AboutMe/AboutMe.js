b14cdd19fcdf5a92b4454fba8414ee6e04efefdb
VictorMtzCodes <victormtzcodes@gmail.com>
2022-02-04 17:46:29 -0500

    finished responsive styling for about me section

--- src/components/Pages/AboutMe/AboutMe.js
+++ src/components/Pages/AboutMe/AboutMe.js
@@ -2,6 +2,7 @@ import './AboutMe.css';
 import Stars from '../../Stars/Stars';
 import Skills from '../../Skills/Skills';
 import { Helmet } from 'react-helmet-async';
+import MainButton from '../../MainButton';
 
 const AboutMe = () => {
     return (
@@ -11,17 +12,18 @@ const AboutMe = () => {
                 <meta name='description' content='About VictorMtzCodes' />
             </Helmet>
             <Stars />
-            <span className="h1-aos" data-aos='fade-up'>
+            <span data-aos='zoom-in'>
             <h1  className='aboutme-title'>ABOUT ME</h1>
             </span>
             <div className="aboutme-content">
-                <div data-aos='fade-up-right' data-aos-delay='200' className="aboutme-info">
+                <div data-aos='zoom-in' className="aboutme-info">
                     <h1 data-aos='fade-up' className='aboutme-subtitle'>Who am I?</h1>
-                    <p data-aos='fade-in' className='aboutme-paragraph'>My name is <span className='name-span'>Victor Martinez Jr. </span>I'm a front end web developer with a hunger for improving. My never quit attitude has allowed me to further develop my passion and skillsets in web development. I love the process of creating a webpage and also the challenge of tackling any hiccups that may occur in the process.</p>
-                    <p data-aos='fade-in'>I learned many traits while teaching myself how to code, such as patience, discipline, problem solving, perseverance and many more. I faced too many challenging obstacles to count but I never quit. I never want to stop growing and improving because I believe I haven't seen the best version of myself.</p>
-                    <p data-aos='fade-in'>I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.</p>
+                    <p className='aboutme-paragraph'>My name is <span className='name-span'>Victor Martinez Jr. </span>I'm a front end web developer with a hunger for improving. My never quit attitude has allowed me to further develop my passion and skillsets in web development. I love the process of creating a webpage and also the challenge of tackling any hiccups that may occur in the process.</p>
+                    <p className='aboutme-second-p'>I learned many traits while teaching myself how to code, such as patience, discipline, problem solving, perseverance and many more. I faced too many challenging obstacles to count but I never quit. I never want to stop growing and improving because I believe I haven't seen the best version of myself.</p>
+                    <p className='aboutme-third-p'>I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.</p>
+                    <MainButton type='main-btn skills-btn' link='#contact' text='CONTACT'/>
                 </div>
-            <div className='skills-section' data-aos='fade-up-left'>
+            <div className='skills-section'>
                 <Skills />
             </div>
             </div>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,44 +1,48 @@
 import './Skills.css';
 import Stars from '../Stars/Stars';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Skills = () => {
     return (
         <section className='skills' id='skills'>
             <Stars />
-            <div className="skills-content">
-                <h1 data-aos='fade-up' className='skills-title'>My Skills</h1>
-                <div data-aos='fade-in' className="skills-images">
+            <div data-aos='zoom-in' className="skills-content">
+                <h1 className='skills-title'>My Skills</h1>
+                <div className='skills-images-container'>
+                <div className="skills-top-images">
                     <div className='skills-logo-container'>
-                        <img className='skills-logo' src='./assets/images/html.svg' alt="html-logo" />
+                        <LazyLoadImage className='skills-logo' src='./assets/images/html.svg' alt="html-logo" />
                         <p className='skills-logo-name'>HTML</p>
                     </div>
                     <div className='skills-logo-container'>
-                        <img className='skills-logo' src='./assets/images/css.svg' alt="css-logo" />
+                        <LazyLoadImage className='skills-logo' src='./assets/images/css.svg' alt="css-logo" />
                         <p className='skills-logo-name'>CSS</p>
                     </div>
                     <div className='skills-logo-container'>
-                        <img className='skills-logo' src='./assets/images/javaScript.svg' alt="js-logo" />
+                        <LazyLoadImage className='skills-logo' src='./assets/images/javaScript.svg' alt="js-logo" />
                         <p className='skills-logo-name'>JavaScript</p>
                     </div>
                     <div className='skills-logo-container'>
-                        <img className='skills-logo' src='./assets/images/react.svg' alt="react-logo" />
+                        <LazyLoadImage className='skills-logo' src='./assets/images/react.svg' alt="react-logo" />
                         <p className='skills-logo-name'>React</p>
                     </div>
                 </div>
-                <div data-aos='fade-in' className='softskills-logos'>
+                <div className='skills-bottom-images'>
                     <div className='skills-logo-container'>
-                        <img className='skills-logo' src='./assets/images/git.svg' alt="git-logo" />
+                        <LazyLoadImage className='skills-logo' src='./assets/images/git.svg' alt="git-logo" />
                         <p className='skills-logo-name'>Git</p>
                     </div>
                     <div className='skills-logo-container'>
-                        <img className='skills-logo' src='./assets/images/github.svg' alt="github-logo" />
+                        <LazyLoadImage className='skills-logo' src='./assets/images/github.svg' alt="github-logo" />
                         <p className='skills-logo-name'>GitHub</p>
                     </div>
                     <div className='skills-logo-container'>
-                        <img className='skills-logo' src='./assets/images/figma.svg' alt="figma-logo" />
+                        <LazyLoadImage className='skills-logo' src='./assets/images/figma.svg' alt="figma-logo" />
                         <p className='skills-logo-name'>Figma</p>
                     </div>
                 </div>
+            </div>
             </div>
         </section>
     )
